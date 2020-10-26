b44997da37a5b860071816d067de4ca3d64abfb0
ray <827154659@qq.com>
2020-10-26 17:31:27 +0800

    循环爬取做完，目前版本一直爬一直爽 #todo： 将文章保存

--- spider.py
+++ spider.py
@@ -1,11 +1,49 @@
 import requests
 import re
 import unicodedata
+import threading
+from queue import Queue
 from bs4 import BeautifulSoup
 from bloom_filter import ScalableBloomFilter
 
 article_url = "https://news.sina.com.cn/s/2020-10-23/doc-iiznezxr7629012.shtml"
 initial_page = "http://news.sina.com.cn/china/"
+
+
+class Spider:
+    url_queue = Queue()
+    bf = ScalableBloomFilter(initial_capacity=10000, error_rate=0.001)
+    max_url_count = 100
+    lock = threading.Lock()
+
+    def __init__(self, max_url=100, init_url=None):
+        self.url_queue.put(init_url)
+        self.max_url_count = max_url
+
+    def scratch_links(self, current_url):
+        res = requests.get(current_url)
+        res.encoding = "utf8"
+        soup = BeautifulSoup(res.text, 'html.parser')
+        text = str(soup.select('a[target="_blank"]'))
+        # 正则获取所有url链接
+        all_links = re.findall(r'(?<=<a href=\").*?(?=\")|(?<=href=\').*?(?=\')', text)
+        for link in all_links:
+            use = re.findall(r'https?://news.sina.com.cn/.*/doc-iiznezxr.*', link)
+            if use:
+                self.lock.acquire()
+                if use[0] not in self.bf:
+                    self.bf.add(use[0])
+                    self.url_queue.put(use[0])
+                self.lock.release()
+
+    def run(self):
+        while not self.url_queue.empty():
+            url = self.url_queue.get()
+            # 爬取当前url
+
+            # 将当前页面的所有链接加入队列
+            self.scratch_links(url)
+            print(url)
 
 
 def article_spider(url):
@@ -25,24 +63,6 @@ def article_spider(url):
     return data
 
 
-def scratch_links(initial_url):
-    url_count = 0
-    page_links = []
-    while url_count <= 5000:
-        res = requests.get(initial_url)
-        res.encoding = "utf8"
-        soup = BeautifulSoup(res.text, 'html.parser')
-        text = str(soup.select('a[target="_blank"]'))
-        # 正则获取所有url链接
-        all_links = re.findall(r'(?<=<a href=\").*?(?=\")|(?<=href=\').*?(?=\')', text)
-        bf = ScalableBloomFilter(initial_capacity=10000, error_rate=0.001)
-        for link in all_links:
-            if link not in bf:
-                bf.add(link)
-                page_links.append(link)
-    return page_links
-
-
 if __name__ == "__main__":
-    result = scratch_links("http://news.sina.com.cn/china/")
-    print(result)
+    spider = Spider(init_url=initial_page)
+    spider.run()
